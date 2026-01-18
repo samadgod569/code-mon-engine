b5c35a920486b486b43f108ea5d6fa428e24cc78
samadgod569 <hellohikaisiho124@gmail.com>
2026-01-18 09:09:17 +0530

    Update ecs.js

--- src/ecs.js
+++ src/ecs.js
@@ -1,39 +1,15 @@
 export class ECS {
-  constructor(){
-    this.entities = new Set();
-    this.components = new Map();
-    this.systems = [];
-    this.nextId = 1;
+  constructor(){ this.entities = new Set(); this.components = new Map(); this.systems = []; this.nextId = 1; }
+  createEntity(){ const id=this.nextId++; this.entities.add(id); return id; }
+  addComponent(entity,comp){
+    const name = comp.constructor.name;
+    if(!this.components.has(name)) this.components.set(name,new Map());
+    this.components.get(name).set(entity,comp);
   }
-
-  createEntity(){
-    const id = this.nextId++;
-    this.entities.add(id);
-    return id;
+  getComponent(entity,name){ return this.components.get(name)?.get(entity); }
+  getEntitiesWith(...names){
+    return [...this.entities].filter(e=>names.every(n=>this.components.get(n)?.has(e)));
   }
-
-  addComponent(entity, component){
-    const type = component.constructor.name;
-    if(!this.components.has(type))
-      this.components.set(type,new Map());
-    this.components.get(type).set(entity,component);
-  }
-
-  getComponent(entity,type){
-    return this.components.get(type)?.get(entity);
-  }
-
-  getEntitiesWith(...types){
-    return [...this.entities].filter(e =>
-      types.every(t=>this.components.get(t)?.has(e))
-    );
-  }
-
-  addSystem(system){
-    this.systems.push(system);
-  }
-
-  update(dt){
-    for(const s of this.systems) s.update(this,dt);
-  }
+  addSystem(sys){ this.systems.push(sys); }
+  update(dt){ for(const sys of this.systems) sys.update(this,dt); }
 }
