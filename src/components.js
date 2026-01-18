87c10d4c858b96c6538aa688c94bfd96571f4c41
samadgod569 <hellohikaisiho124@gmail.com>
2026-01-18 12:41:07 +0530

    Update components.js

--- src/components.js
+++ src/components.js
@@ -1,6 +1,36 @@
-export class Transform { constructor(x=0,y=0,z=0){ this.position={x,y,z}; } }
-export class MeshRenderer { constructor(mesh){ this.mesh = mesh; } }
-export class Velocity { constructor(x=0,y=0,z=0){ this.x=x; this.y=y; this.z=z; } }
-export class PlayerControlled { constructor(){ this.input={up:0,down:0,left:0,right:0}; } }
-export class AIControlled { constructor(speed=1){ this.speed=speed; } }
-export class Collider { constructor(size=1){ this.size=size; } }
+export class Transform{
+ constructor(x=0,y=0,z=0){
+  this.x=x; this.y=y; this.z=z;
+  this.rx=0; this.ry=0; this.rz=0;
+  this.s=1;
+ }
+}
+
+export class Velocity{
+ constructor(x=0,y=0,z=0){
+  this.x=x; this.y=y; this.z=z;
+ }
+}
+
+export class Sprite2D{
+ constructor(w=40,h=40,color="white"){
+  this.w=w; this.h=h; this.color=color;
+ }
+}
+
+export class Mesh3D{
+ constructor(mesh){ this.mesh=mesh; }
+}
+
+export class Player{}
+export class Enemy{}
+
+export class Collider{
+ constructor(w=1,h=1,d=1){
+  this.w=w; this.h=h; this.d=d;
+ }
+}
+
+export class Script{
+ constructor(fn){ this.fn=fn; }
+}
