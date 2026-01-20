9abf819ec5971b1b738ce60c3b18ad0dc57f5f0f
samadgod569 <hellohikaisiho124@gmail.com>
2026-01-20 18:57:44 +0530

    Update animation.js

--- systems/animation.js
+++ systems/animation.js
@@ -1,13 +1,37 @@
+// animation.js
 export class AnimationSystem{
+ constructor(){
+  this.states=new Map();
+ }
+
  update(w,dt){
   for(const e of w.with("Animation")){
    const a=w.get(e,"Animation");
-   if(!a.playing) continue;
+   if(!a.playing||a.frames.length===0) continue;
+
    a.time+=dt;
-   if(a.time>a.speed){
+   if(a.time>=a.speed){
     a.time=0;
-    a.current=(a.current+1)%a.frames.length;
+    a.current++;
+    if(a.current>=a.frames.length){
+     if(a.loop) a.current=0;
+     else{ a.current=a.frames.length-1; a.playing=false; }
+    }
    }
   }
+ }
+
+ setState(e,state){
+  this.states.set(e,state);
+ }
+
+ stop(w,e){
+  const a=w.get(e,"Animation");
+  if(a) a.playing=false;
+ }
+
+ play(w,e){
+  const a=w.get(e,"Animation");
+  if(a) a.playing=true;
  }
 }
